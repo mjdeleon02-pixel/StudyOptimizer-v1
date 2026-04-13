062f6d3517eacf3f9ecc85641cda68780480e1e3
Justine <mjdeleon02@tip.edu.ph>
2026-04-13 12:43:30 +0800

    Auto-create superadmin in build script

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ python -m pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='superadmin@gmail.com').exists() or User.objects.create_superuser('superadmin@gmail.com', 'superadmin@gmail.com', 'admin123!')"
