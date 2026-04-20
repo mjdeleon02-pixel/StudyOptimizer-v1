48e02d6d26952b98ac0d20e70f6577128146d9b0
Roella Monzaga <mrmonzaga@tip.edu.ph>
2026-04-20 10:28:41 +0800

    fix superuser creation

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Create superuser if it doesn't exist
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='superadmin@gmail.com').exists() or User.objects.create_superuser('superadmin@gmail.com', 'superadmin@gmail.com', 'admin123!')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='superadmin@gmail.com').exists() or User.objects.create_superuser('superadmin@gmail.com', 'superadmin@gmail.com', 'StudyAdmin@2025!')"
 
