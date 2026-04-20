3648e13e835ea1d24731d2c21cb002d628adf2ce
Roella Monzaga <mrmonzaga@tip.edu.ph>
2026-04-20 10:40:53 +0800

    force recreate superuser"

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Create superuser if it doesn't exist
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='superadmin@gmail.com').exists() or User.objects.create_superuser('superadmin@gmail.com', 'superadmin@gmail.com', 'StudyAdmin@2025!')"
+python manage.py shell -c " from django.contrib.auth import get_user_model User = get_user_model() User.objects.filter(email='superadmin@gmail.com').delete() u = User.objects.create_superuser('superadmin', 'superadmin@gmail.com', 'StudyAdmin@2025!') print('Superuser created:', u.email)"
 
